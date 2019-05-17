c46d3e4e41940b77d612f070f18410b84d7afb4a
Melsm650 <wp.developerhelpers@gmail.com>
2019-05-17 15:11:15 -0700

    SingleTask.jsx - implimented connection and delete click function

--- client/src/components/SingleTask.jsx
+++ client/src/components/SingleTask.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
 import {Button, CardBody, Card} from 'reactstrap'
 
+//to connect to store
+import { connect } from 'react-redux';
+import {deletePost} from '../../actions/postAction.jsx'
+
 class SingleTask extends Component {
+  
+  deleteTask(id) {
+    this.props.deletePost(id);
+  }
   render() {
     const {id, title, value, aproved, parent_id} = this.props.post;
     return (
@@ -11,10 +19,11 @@ class SingleTask extends Component {
             <li><strong>{title}</strong></li>
             <li> Pts. {value}</li>
             </ul>
-            <Button>Delete</Button> <Button>update</Button>
+            <Button onClick={()=> this.deleteTask(id)}>Delete</Button> <Button>update</Button>
         </CardBody>
     </Card>
     )
   }
 }
-export default SingleTask;
+
+export default connect (null, {deletePost})(SingleTask);
