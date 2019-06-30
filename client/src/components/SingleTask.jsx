9a8000bb366cc2231cee3c045127d0bd4993908b
code-mel <code.devmel@gmail.com>
2019-06-30 14:50:13 -0700

    "SINGLETASK.jsk - submit task object updated | CurrentCard based on status and aproved value "

--- client/src/components/SingleTask.jsx
+++ client/src/components/SingleTask.jsx
@@ -38,7 +38,8 @@ class SingleTask extends Component {
       value : this.state.value,
       aproved : this.state.aproved,
       status : this.props.post.status,
-      parent_id : this.props.post.parent_id, 
+      parent_id : this.props.post.parent_id,
+      child_id : 'NULL', 
       id: this.state.id
     }
     // Call Action and past post through
@@ -52,8 +53,12 @@ class SingleTask extends Component {
     })
   
   }
+  statusUpdate(id, stats) {
+    ///Status update function ....
+  }
   render() {
     const {id, title, value, aproved,status, parent_id} = this.props.post;
+    let currentCard;
 
     let updateFrom = (<CardBody>
         <form onSubmit={this.submitHandler} className='update-form'>
@@ -64,17 +69,41 @@ class SingleTask extends Component {
         </form>
       </CardBody>);
 
-    let card = (<CardBody>
-      <ul className="card-details">
-        <li><strong>{title}</strong></li>
-        <li> Pts. {value}</li>
-    </ul>
-    <Button onClick={()=> this.deleteTask(id)}>Delete</Button> <Button name="updateToggle" onClick={this.stateHandler}>update</Button>
-    </CardBody>);
+/// ~~~~~ Boolean statments that help determine the current card that should be displayed based  on status/aproved values ~~~~~~~ ///
+
+    if(status === 1 || status === 0) { // if status is 0 or 1 then card with delete and update button will be visibale 
+      currentCard = (<CardBody>
+        <p>Status: {status ? 'In the works' : 'Open'}</p> 
+        <ul className="card-details">
+          <li><strong>{title}</strong></li>
+          <li> Pts. {value}</li>
+      </ul>
+      <Button onClick={()=> this.deleteTask(id)}>Delete</Button> <Button name="updateToggle" onClick={this.stateHandler}>update</Button>
+      </CardBody>);
+    }else if(status === 2 && !aproved){ // if status is 2 the card with dissaprove or approve button will display
+      currentCard = (<CardBody>
+        <p>Status: Needs Approval</p>
+        <ul className="card-details">
+          <li><strong>{title}</strong></li>
+          <li> Pts. {value}</li>
+      </ul>
+      <Button onClick={()=> this.statusUpdate(id, 0)}>Disapprove</Button> <Button name="approveTask" onClick={()=> this.statusUpdate(id, 1)}>Approve</Button>
+      </CardBody>);
+    }else if(status === 2 && aproved){ // if status is 2 and 1 then card with just Delete button will show
+      currentCard = (<CardBody>
+        <p>Status: Done</p>
+        <ul className="card-details">
+          <li><strong>{title}</strong></li>
+          <li> Pts. {value}</li>
+      </ul>
+      <Button onClick={()=> this.deleteTask(id)}>Delete</Button>
+      </CardBody>);
+    }
+/// ~~~~ End of statments ~~~ ///
 
     return (
     <Card key={id}>
-        {this.state.updateToggle ? updateFrom : card}
+        {this.state.updateToggle ? updateFrom : currentCard}
     </Card>
     )
   }
